aaa45e5d036e690d46fa0b68134a11c59cf03074
iPiotr <ip.starzec@gmail.com>
2020-12-05 00:25:15 +0100

    upgrade

--- app.js
+++ app.js
@@ -2,15 +2,88 @@
 //BUDGET CONTROLLER
 const budgetController = (function() {
 
-    //code
+     const Expense = function(id, description, value) {
+         this.id = id;
+         this.description = description;
+         this.value = value;
+     };
+
+     const Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const allExpanses = [];
+    const allIncomes = [];
+    const totalExpanses = 0;
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            
+            let newItem, ID;
+
+            //create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Create new item based on 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem =  new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //Push it into our data structure
+            data.allItems[type].push(newItem);
+            return newItem;
+
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
-
 
 //UI CONTROLLER
 const UIcontroller = (function() {
 
-    //code
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -18,24 +91,43 @@ const UIcontroller = (function() {
 //GLOBAL APP CONTROLLER
 const controller = (function(budgetCtrl, UICtrl) {
 
+    const setupEventListeners = function() {
+        const DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+        
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+ 
+        });
+
+    };
+
+
+
     const ctrlAddItem = function() {
+        let input, newItem;
         //1. Get the field input data
+        input = UICtrl.getInput();
+
         //2. Add the item to the budet controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         //3. Add the item to the UI
         //4. Calculate the budget
         //5. Display the budget on the UI
-        console.log('It works');
-    }
+     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
- 
-    });
+     return {
+         init: function() {
+             console.log('application has started.');
+             setupEventListeners();
+         }
+     }
 
 })(budgetController, UIcontroller); 
 
+controller.init();
